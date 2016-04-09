8fa769dfc6ec7516492c3afa19d21f47d2142cf7
sduduzog <gumedesduduzo@gmail.com>
2016-04-09 04:52:22 +0200

    modified home and login layout

--- app/src/main/java/com/sbuhost/easyticket/Home.java
+++ app/src/main/java/com/sbuhost/easyticket/Home.java
@@ -2,8 +2,6 @@ package com.sbuhost.easyticket;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 /**
