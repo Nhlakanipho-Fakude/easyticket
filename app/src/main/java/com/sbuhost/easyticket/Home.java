800ca2131e315f3dc1687d0742aa0f612b9730ed
Awesome30 <ncfakude33@gmail.com>
2016-04-09 04:48:17 +0200

    added the buttons to the Login activity

--- app/src/main/java/com/sbuhost/easyticket/Home.java
+++ app/src/main/java/com/sbuhost/easyticket/Home.java
@@ -1,7 +1,20 @@
 package com.sbuhost.easyticket;
 
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 /**
  * Created by Nhlakanipho Fakude on 4/9/2016.
  */
-public class Home {
+public class Home extends ActionBarActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+        Toast.makeText(getApplicationContext(),"Welcome Home",Toast.LENGTH_LONG).show();
+    }
 }
