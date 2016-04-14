0d90293094bacdfe1690ec5fdd1df2fc2723ee05
sduduzog <gumedesduduzo@gmail.com>
2016-04-14 21:17:57 +0200

    remade login screen

--- app/src/main/java/com/sbuhost/easyticket/Login.java
+++ app/src/main/java/com/sbuhost/easyticket/Login.java
@@ -1,27 +1,16 @@
 package com.sbuhost.easyticket;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
-public class Login extends ActionBarActivity {
-private Button loginB,signupB,forgotB;
-private EditText email,password;
+public class Login extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        loginB=(Button) findViewById(R.id.login);
-        signupB=(Button) findViewById(R.id.signup);
-        forgotB=(Button) findViewById(R.id.forgot);
-        email=(EditText) findViewById(R.id.email);
-        password=(EditText) findViewById(R.id.password);
-
-
     }
     public void login(View v) {
         startActivity(new Intent(Login.this,Item.class));
