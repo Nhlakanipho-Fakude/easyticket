f36a2e5ba597529ec823eb0eabe7185e60bf3173
sduduzog <gumedesduduzo@gmail.com>
2016-04-20 01:51:09 +0200

    made a few mods

--- app/src/main/java/com/sbuhost/easyticket/Item.java
+++ app/src/main/java/com/sbuhost/easyticket/Item.java
@@ -1,14 +1,23 @@
 package com.sbuhost.easyticket;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
 
 public class Item extends Activity {
 
+    private HorizontalListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.test_layout);
+        setContentView(R.layout.activity_item);
+        listView = (HorizontalListView) findViewById(R.id.hListView);
+        ThumbItemAdapter adapter = new ThumbItemAdapter(this);
+        listView.setAdapter((ListAdapter)adapter);
     }
+    public void buy(View v){}
 }
---
+++ app/src/main/java/com/sbuhost/easyticket/ThumbItemAdapter.java
@@ -0,0 +1,52 @@
+package com.sbuhost.easyticket;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ThumbItemAdapter extends BaseAdapter{
+
+    int[] imageId = {R.mipmap.poster_background, R.mipmap.poster_background, R.mipmap.poster_background, R.mipmap.poster_background};
+
+    String [] result;
+    Context context;
+    private static LayoutInflater inflater=null;
+    public ThumbItemAdapter(Context context) {
+        // TODO Auto-generated constructor stub
+        this.context = context;
+        inflater = ( LayoutInflater )context.
+                getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+    @Override
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return imageId.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        // TODO Auto-generated method stub
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        // TODO Auto-generated method stub
+        return position;
+    }
+    public class Holder{
+        private TextView title, date, description;
+        private ImageView poster;
+    }
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        if(convertView==null)
+            view = inflater.inflate(R.layout.item_thumb, parent, false);
+        return view;
+    }
+
+}
