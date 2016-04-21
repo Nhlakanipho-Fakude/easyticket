71745199fd5851d3806b075c8fafc9579a56ff43
sduduzog <gumedesduduzo@gmail.com>
2016-04-21 13:34:57 +0200

    Added fragments for dialog

--- app/src/main/java/com/sbuhost/easyticket/Item.java
+++ app/src/main/java/com/sbuhost/easyticket/Item.java
@@ -1,9 +1,13 @@
 package com.sbuhost.easyticket;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListAdapter;
@@ -19,5 +23,19 @@ public class Item extends Activity {
         ThumbItemAdapter adapter = new ThumbItemAdapter(this);
         listView.setAdapter((ListAdapter)adapter);
     }
-    public void buy(View v){}
+    public void buy(View v){
+
+    }
+    public void loadPoster(View v){
+
+    }
+    public class FireMissilesDialogFragment extends DialogFragment {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the Builder class for convenient dialog construction
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            // Create the AlertDialog object and return it
+            return builder.create();
+        }
+    }
 }
