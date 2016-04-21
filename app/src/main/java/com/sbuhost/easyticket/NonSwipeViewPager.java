632fd788a6df69e5893b182f8078fd0602187199
sduduzog <gumedesduduzo@gmail.com>
2016-04-21 15:00:31 +0200

    sync

---
+++ app/src/main/java/com/sbuhost/easyticket/NonSwipeViewPager.java
@@ -0,0 +1,26 @@
+package com.sbuhost.easyticket;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+public class NonSwipeViewPager extends ViewPager{
+    public NonSwipeViewPager(Context context) {
+        super(context);
+    }
+
+    public NonSwipeViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+/*
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        return !super.onInterceptTouchEvent(event);
+    }*/
+}
--- app/src/main/java/com/sbuhost/easyticket/PurchaseActivity.java
+++ app/src/main/java/com/sbuhost/easyticket/PurchaseActivity.java
@@ -1,13 +1,61 @@
 package com.sbuhost.easyticket;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 
 public class PurchaseActivity extends AppCompatActivity {
+    private final static int NUM_PAGES = 2;
+    private ViewPager mPager;
+    private PagerAdapter mPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_purchase);
+        // Instantiate a ViewPager and a PagerAdapter.
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {return true;}
+        });
+        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mPager.getCurrentItem() == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed();
+        } else {
+            // Otherwise, select the previous step.
+            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+        }
+    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            if (position == 0)
+                return new ItemOptionsFragment();
+            return new PaymentFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
     }
 }
